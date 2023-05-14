c244215fb3de4426f5a3e86a6d7aa508b5ff2f41
Jesus Gutierrez <jesusg.1301.jg@gmail.com>
2023-05-14 19:21:49 -0400

    adding cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const cors = require('cors');
 const Server = require("./models/server");
 if (process.env.NODE_ENV != "production") {
     require("dotenv").config();
   }
-const server = new Server();
 
+const server = new Server();
+// Enable CORS
+server.use(cors());
 server.exe()
